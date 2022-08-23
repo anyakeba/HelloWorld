ea0a14fad6decb039f39d2f331d2ceaf1542187e
Anna Keba <anyakeba@gmail.com>
2022-08-23 16:16:10 +0300

    feature added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main (String[] args){
         System.out.println ("Hello World!");
         System.out.println ("My name Anna");
+        System.out.println ("feature added");
     }
 }
